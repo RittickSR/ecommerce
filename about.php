7163f3de3b6970ad264664326dbea964491ce1f3
Hariharan Pillai <haripillaide@gmail.com>
2020-01-13 23:07:37 +0530

    added sample abt contact

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+    <div class="content-wrapper">
+	    <div class="container">
+
+        <h1>
+
+            About TRIFED</h1>
+            The Tribal Cooperative Marketing Development Federation of India (TRIFED) came into existence in 1987. It is a national-level apex organization functioning under the administrative control of Ministry of Tribal Affairs, Govt. of India. TRIFED has its registered and Head Office located in New Delhi and has a network of 13 Regional Offices located at various places in the country.
+
+	      <section class="content">
+     </div>
+    </div>
+    <?php include 'includes/footer.php'; ?>
+    
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+    <div class="content-wrapper">
+	    <div class="container">
+        <section class="content">
+        <h1>
+
+            Contact Us</h1>
+            Tribal Co-Operative Marketing Development Federation of India Limited (TRIFED)<br>
+Head Office, NCUI Building,<br>
+2nd Floor, 3, Siri Institutional Area,<br>
+August Kranti Marg,<br>
+New Delhi-110016.<br>
+Ph.-091(11) 26569064,26968247<br>
+Fax. - 091(11)26866149<br>
+Email: trifed@rediffmail.com<br>
+	      
+     </div>
+    </div>
+    <?php include 'includes/footer.php'; ?>
+    
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
