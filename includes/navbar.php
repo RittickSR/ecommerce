efc0403c07ef3c34aa65c5346c38ad1418b92e7c
Hariharan Pillai <haripillaide@gmail.com>
2020-01-14 14:00:22 +0530

    multi lang

--- includes/navbar.php
+++ includes/navbar.php
@@ -129,6 +129,18 @@
               ";
             }
           ?>
+          <li>
+          <div id="google_translate_element"></div>
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+          </li>
         </ul>
       </div>
     </div>
